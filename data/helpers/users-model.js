4645201c7e56c724bde04dcc0ef2bb09a6f72e8b
raythurman2386 <raymondthurman5@gmail.com>
2020-01-20 20:54:36 -0500

    users-model templated initial

--- data/helpers/users-model.js
+++ data/helpers/users-model.js
@@ -1,9 +1,34 @@
+const db = require('../db-config');
+
 // find
+function find() {
+  return db('users').select();
+}
 
 // findBy
+function findBy(filter) {
+  return db('users').where(filter).first();
+}
 
 // add
+function add(user) {
+
+}
 
 // update
+function update(user, id) {
+
+}
 
 // remove
+function remove(id) {
+
+}
+
+module.exports = {
+  find,
+  findBy,
+  add,
+  update,
+  remove
+}
