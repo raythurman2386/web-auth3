6b0131805af236466a53463807ba81978fe84677
raythurman2386 <raymondthurman5@gmail.com>
2020-01-20 20:26:18 -0500

    basic server set up

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+server.use(cors());
+server.use(helmet());
+
+server.use('/', (req, res) => {
+  res.json({ message: "Server working." })
+})
+
+module.exports = server;
