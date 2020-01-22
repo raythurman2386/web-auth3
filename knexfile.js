ed76825e47f8bbb52da741be24e324e9df904d74
raythurman2386 <raymondthurman5@gmail.com>
2020-01-21 21:31:52 -0500

    refactored knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,22 +16,6 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
     connection: {
