7ca372c2ac66a3749ba8beedeb0ff3e6f769dca0
raythurman2386 <raymondthurman5@gmail.com>
2020-01-21 20:05:17 -0500

    server set up with all routes

--- api/server.js
+++ api/server.js
@@ -2,13 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const authRouter = require('./routes/auth-router');
+const userRouter = require('./routes/user-router');
+
 const server = express();
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
+server.use('/auth', authRouter);
+server.use('/api', userRouter);
+
 server.use('/', (req, res) => {
   res.json({ message: "Server working." })
 });
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "Something went wrong, I apologize."
+  })
+})
 
 module.exports = server;
